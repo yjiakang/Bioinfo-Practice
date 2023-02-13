e1f9883f7bb120e3b0052f34a860850d7c1ed0aa
yjiakang <49177016+yjiakang@users.noreply.github.com>
2023-02-13 19:57:57 +0800

    Update derep.py

--- derep.py
+++ derep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 
 #-*-coding : utf-8 -*-
 import argparse
-parser = argparse.ArgumentParser(description = "\nThis python script is used to extrac the first line among all the line with same start", add_help = False, usage = "\n python3 derep.py -i [input] -o [output] \n python3 seq_split.py --input [input] --output [output]")
+parser = argparse.ArgumentParser(description = "\nThis python script is used to extrac the first line among all the line with same start", add_help = False, usage = "\n python3 derep.py -i [input] -o [output] \n python3 derep.py --input [input] --output [output]")
 required = parser.add_argument_group("Required options")
 optional = parser.add_argument_group("Optional options")
 required.add_argument("-i", "--input", metavar = "[input]", help = "input file", required = True)
